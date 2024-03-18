a96820776b921c137a1f24193996382c30038e2b
Allotei <allotei5@gmail.com>
2024-03-18 16:48:41 -0300

    google translate widget

---
+++ src/components/GoogleTranslate.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+
+const GoogleTranslate = () => {
+  useEffect(() => {
+    // Load the Google Translate script
+    const googleTranslateElementInit = () => {
+      new window.google.translate.TranslateElement({
+        pageLanguage: 'en', // or any default language
+        
+      }, 'google_translate_element');
+    };
+
+    const script = document.createElement('script');
+    script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    script.async = true;
+    document.body.appendChild(script);
+
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
+
+  return <div id="google_translate_element"></div>;
+};
+
+export default GoogleTranslate;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 import logo from '../assets/logo@2x.png'
+import GoogleTranslate from './GoogleTranslate'
 
 
 const NavBar = () => {
   return (
     <div className='p-5 mx:10 lg:mx-48 min-h-screen'>
-        <div>
-            <div className='w-1/5'>
+        <div className='flex justify-between'>
+            <div className='md:w-2/5'>
                 <img src={logo} width="30%"/>
             </div>
-            <div></div>
+            <div>
+              <GoogleTranslate />
+            </div>
         </div>
         <Outlet />
     </div>
